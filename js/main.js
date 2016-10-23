2365393b0fe2404fe7bf2faff267eca493b2f0bb
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2016-10-23 17:23:34 -0400

    Put input methods back in main
    keyEvents should be injected into the game object, but it still needs to be a global

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 var game;
+var keyEvents;
 
 window.onload = function() {
   var canvas = document.getElementById('gameCanvas');
   var graphics = new Graphics(canvas);
 
+  keyEvents = new KeyEvents();
+  document.addEventListener("keydown", keyPressed);
+  document.addEventListener("keyup", keyReleased);
+
   var firstLevel = new window[firstLevelClass];
   game = new window[gameClass](graphics, firstLevel);
 
-  setupInput();
   setInterval(perFrame, 1000 / game.FRAMES_PER_SECOND);
 }
+
+function keyPressed(evt) {
+  keyEvents.setKeyHoldState(evt.keyCode, true);
+  evt.preventDefault(); // without this, arrow keys scroll the browser!
+};
+
+function keyReleased(evt) {
+  keyEvents.setKeyHoldState(evt.keyCode, false);
+};
 
 function perFrame() {
   game.perFrame();
