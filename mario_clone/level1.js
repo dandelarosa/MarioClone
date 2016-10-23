a453964a48e64ac26381f1de559b1819e093a5aa
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2016-10-23 16:29:00 -0400

    Make jumperMove an instance variable

--- mario_clone/level1.js
+++ mario_clone/level1.js
@@ -36,48 +36,6 @@ function isBrickAtPixelCoord(hitPixelX, hitPixelY) {
   return (brickGrid[brickIndex] == 1);
 }
 
-function jumperMove() {
- if(jumperOnGround) {
-    jumperSpeedX *= GROUND_FRICTION;
-  } else {
-    jumperSpeedX *= AIR_RESISTANCE;
-    jumperSpeedY += GRAVITY;
-    if(jumperSpeedY > JUMPER_RADIUS) { // cheap test to ensure can't fall through floor
-      jumperSpeedY = JUMPER_RADIUS;
-    }
-  }
-
-  if(holdLeft) {
-    jumperSpeedX = -RUN_SPEED;
-  }
-  if(holdRight) {
-    jumperSpeedX = RUN_SPEED;
-  }
-
-  if(jumperSpeedY < 0 && isBrickAtPixelCoord(jumperX, jumperY - JUMPER_RADIUS) == 1) {
-    jumperY = (Math.floor(jumperY / BRICK_H)) * BRICK_H + JUMPER_RADIUS;
-    jumperSpeedY = 0.0;
-  }
-
-  if(jumperSpeedY > 0 && isBrickAtPixelCoord(jumperX, jumperY + JUMPER_RADIUS) == 1) {
-    jumperY = (1 + Math.floor(jumperY / BRICK_H)) * BRICK_H - JUMPER_RADIUS;
-    jumperOnGround = true;
-    jumperSpeedY = 0;
-  } else if(isBrickAtPixelCoord(jumperX, jumperY + JUMPER_RADIUS + 2) == 0) {
-    jumperOnGround = false;
-  }
-
-  if(jumperSpeedX < 0 && isBrickAtPixelCoord(jumperX-JUMPER_RADIUS,jumperY) == 1) {
-    jumperX = (Math.floor(jumperX / BRICK_W)) * BRICK_W + JUMPER_RADIUS;
-  }
-  if(jumperSpeedX > 0 && isBrickAtPixelCoord(jumperX + JUMPER_RADIUS, jumperY) == 1) {
-    jumperX = (1 + Math.floor(jumperX / BRICK_W)) * BRICK_W - JUMPER_RADIUS;
-  }
-
-  jumperX += jumperSpeedX; // move the jumper based on its current horizontal speed
-  jumperY += jumperSpeedY; // same as above, but for vertical
-}
-
 function Level1() {
   this.jumperReset();
 };
@@ -88,7 +46,49 @@ Level1.prototype.jumperReset = function() {
 };
 
 Level1.prototype.update = function() {
-  jumperMove();
+  this.jumperMove();
+};
+
+Level1.prototype.jumperMove = function() {
+  if(jumperOnGround) {
+     jumperSpeedX *= GROUND_FRICTION;
+   } else {
+     jumperSpeedX *= AIR_RESISTANCE;
+     jumperSpeedY += GRAVITY;
+     if(jumperSpeedY > JUMPER_RADIUS) { // cheap test to ensure can't fall through floor
+       jumperSpeedY = JUMPER_RADIUS;
+     }
+   }
+
+   if(holdLeft) {
+     jumperSpeedX = -RUN_SPEED;
+   }
+   if(holdRight) {
+     jumperSpeedX = RUN_SPEED;
+   }
+
+   if(jumperSpeedY < 0 && isBrickAtPixelCoord(jumperX, jumperY - JUMPER_RADIUS) == 1) {
+     jumperY = (Math.floor(jumperY / BRICK_H)) * BRICK_H + JUMPER_RADIUS;
+     jumperSpeedY = 0.0;
+   }
+
+   if(jumperSpeedY > 0 && isBrickAtPixelCoord(jumperX, jumperY + JUMPER_RADIUS) == 1) {
+     jumperY = (1 + Math.floor(jumperY / BRICK_H)) * BRICK_H - JUMPER_RADIUS;
+     jumperOnGround = true;
+     jumperSpeedY = 0;
+   } else if(isBrickAtPixelCoord(jumperX, jumperY + JUMPER_RADIUS + 2) == 0) {
+     jumperOnGround = false;
+   }
+
+   if(jumperSpeedX < 0 && isBrickAtPixelCoord(jumperX-JUMPER_RADIUS,jumperY) == 1) {
+     jumperX = (Math.floor(jumperX / BRICK_W)) * BRICK_W + JUMPER_RADIUS;
+   }
+   if(jumperSpeedX > 0 && isBrickAtPixelCoord(jumperX + JUMPER_RADIUS, jumperY) == 1) {
+     jumperX = (1 + Math.floor(jumperX / BRICK_W)) * BRICK_W - JUMPER_RADIUS;
+   }
+
+   jumperX += jumperSpeedX; // move the jumper based on its current horizontal speed
+   jumperY += jumperSpeedY; // same as above, but for vertical
 };
 
 Level1.prototype.draw = function(graphics) {
