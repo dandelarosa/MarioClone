2728a87eccb784516c2cfbec275d6dd6f754829d
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2016-10-23 20:12:05 -0400

    Make cameraFollow a Camera method

--- zelda_clone/camera.js
+++ zelda_clone/camera.js
@@ -7,3 +7,45 @@ function Camera() {
   this.thresholdFromCenterX = 0;
   this.thresholdFromCenterY = 0;
 };
+
+Camera.prototype.follow = function(object, levelDimensions) {
+  var cameraFocusCenterX = this.panX + this.width / 2;
+  var cameraFocusCenterY = this.panY + this.height / 2;
+
+  var playerDistFromCameraFocusX = Math.abs(object.x - cameraFocusCenterX);
+  var playerDistFromCameraFocusY = Math.abs(object.y - cameraFocusCenterY);
+
+  if (playerDistFromCameraFocusX > this.thresholdFromCenterX) {
+    if (cameraFocusCenterX < object.x) {
+      this.panX += this.speed;
+    }
+    else {
+      this.panX -= this.speed;
+    }
+  }
+  if (playerDistFromCameraFocusY > this.thresholdFromCenterY) {
+    if(cameraFocusCenterY < object.y) {
+      this.panY += this.speed;
+    }
+    else {
+      this.panY -= this.speed;
+    }
+  }
+
+  // this next code blocks the game from showing out of bounds
+  // (this isn't required, if you don't mind seeing beyond edges)
+  if (this.panX < 0) {
+    this.panX = 0;
+  }
+  if (this.panY < 0) {
+    this.panY = 0;
+  }
+  var maxPanRight = levelDimensions.width - this.width;
+  var maxPanTop = levelDimensions.height - this.height;
+  if (this.panX > maxPanRight) {
+    this.panX = maxPanRight;
+  }
+  if (this.panY > maxPanTop) {
+    this.panY = maxPanTop;
+  }
+}
--- zelda_clone/level1.js
+++ zelda_clone/level1.js
@@ -14,53 +14,13 @@ function Level1() {
 
 Level1.prototype.update = function(keyEvents) {
   this.slider.move(keyEvents, this.bricks);
-  this.cameraFollow(this.slider, this.levelDimensions());
+  this.camera.follow(this.slider, this.levelDimensions());
 };
 
 Level1.prototype.levelDimensions = function() {
   return {
     width: BRICK_COLS * BRICK_W,
     height: BRICK_ROWS * BRICK_H
-  }
-}
-
-Level1.prototype.cameraFollow = function(object, levelDimensions) {
-  var cameraFocusCenterX = this.camera.panX + this.camera.width / 2;
-  var cameraFocusCenterY = this.camera.panY + this.camera.height / 2;
-
-  var playerDistFromCameraFocusX = Math.abs(object.x - cameraFocusCenterX);
-  var playerDistFromCameraFocusY = Math.abs(object.y - cameraFocusCenterY);
-
-  if(playerDistFromCameraFocusX > this.camera.thresholdFromCenterX) {
-    if(cameraFocusCenterX < object.x)  {
-      this.camera.panX += this.camera.speed;
-    } else {
-      this.camera.panX -= this.camera.speed;
-    }
-  }
-  if(playerDistFromCameraFocusY > this.camera.thresholdFromCenterY) {
-    if(cameraFocusCenterY < object.y)  {
-      this.camera.panY += this.camera.speed;
-    } else {
-      this.camera.panY -= this.camera.speed;
-    }
-  }
-
-  // this next code blocks the game from showing out of bounds
-  // (this isn't required, if you don't mind seeing beyond edges)
-  if(this.camera.panX < 0) {
-    this.camera.panX = 0;
-  }
-  if(this.camera.panY < 0) {
-    this.camera.panY = 0;
-  }
-  var maxPanRight = levelDimensions.width - this.camera.width;
-  var maxPanTop = levelDimensions.height - this.camera.height;
-  if(this.camera.panX > maxPanRight) {
-    this.camera.panX = maxPanRight;
-  }
-  if(this.camera.panY > maxPanTop) {
-    this.camera.panY = maxPanTop;
   }
 }
 
