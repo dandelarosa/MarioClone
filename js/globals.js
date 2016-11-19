8631d9bcea1f182f5a6b6d8f708810aa44c1071b
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2016-11-19 17:04:53 -0500

    Move Keyboard to input folder

--- js/globals.js
+++ js/globals.js
@@ -2,6 +2,7 @@ var globals = new Globals();
 
 function Globals() {
   var globals = {
+    keyboard: null,
     mouse: null,
     setup: setup,
   };
@@ -11,9 +12,21 @@ function Globals() {
     var canvas = document.getElementById('gameCanvas');
     globals.graphics = new Graphics(canvas);
 
+    globals.keyboard = new Keyboard();
+    document.addEventListener("keydown", gameKeydown);
+    document.addEventListener("keyup", gameKeyup);
+
     globals.mouse = new Mouse(canvas, document.documentElement);
     canvas.addEventListener('mousemove', gameMousemove);
   }
+
+  function gameKeydown(evt) {
+    globals.keyboard.keydown(evt);
+  };
+
+  function gameKeyup(evt) {
+    globals.keyboard.keyup(evt);
+  };
 
   function gameMousemove(evt) {
     globals.mouse.update(evt);
