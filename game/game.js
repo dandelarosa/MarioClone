68092cf15c1e96f57b44423852ead19653bfdf78
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2016-11-20 18:21:48 -0500

    Make the Game responsible for making the Grid
    This makes the editor form able to modify the grid directly

--- game/game.js
+++ game/game.js
@@ -4,9 +4,38 @@ function Game() {
   this.height = 240;
   this.scaleX = 2;
   this.scaleY = 2;
-  this.bricks = new Bricks();
-  this.camera = new Camera(0, 0, this.width, this.height);
+  this.gridData = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+    1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1,
+    1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+    1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1,
+    1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ];
+  this.reset();
 };
+
+Game.prototype.reset = function() {
+  var gridBuilder = {
+    cellWidth: 16,
+    cellHeight: 16,
+    data: this.gridData,
+    numCols: 20,
+    numRows: 15,
+  };
+  var grid = new Grid(gridBuilder);
+  this.bricks = new Bricks(grid);
+  this.camera = new Camera(0, 0, this.width, this.height);
+}
 
 Game.prototype.update = function() {
   this.camera.update(globals.keyboard, this.bricks);
