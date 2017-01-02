b4db1a7746176cae26d8601cdc468cce0c87993a
Dan Dela Rosa <dandelarosa@Dans-MBP.nyc.rr.com>
2017-01-02 13:03:39 -0500

    Draw tile image by default

--- game/bricks/tiledrawing.js
+++ game/bricks/tiledrawing.js
@@ -2,32 +2,13 @@ function drawTile(graphics, value, x, y) {
   switch (value) {
     case TILE_EMPTY:
     case TILE_OUT_OF_BOUNDS:
+    case undefined:
       break;
     case TILE_BLUE_SKY:
       graphics.fillRect(x , y, 16, 16, '#aaccff');
       break;
-    case TILE_BROWN_GROUND:
-      drawTileImage(graphics, value, x, y);
-      break;
-    case TILE_CLOUD_UL:
-      drawTileImage(graphics, value, x, y);
-      break;
-    case TILE_CLOUD_UC:
-      drawTileImage(graphics, value, x, y);
-      break;
-    case TILE_CLOUD_UR:
-      drawTileImage(graphics, value, x, y);
-      break;
-    case TILE_CLOUD_LL:
-      drawTileImage(graphics, value, x, y);
-      break;
-    case TILE_CLOUD_LC:
-      drawTileImage(graphics, value, x, y);
-      break;
-    case TILE_CLOUD_LR:
-      drawTileImage(graphics, value, x, y);
-      break;
     default:
+      drawTileImage(graphics, value, x, y);
       break;
   }
 };
