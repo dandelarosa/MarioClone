0ce26d14c0a94325144d726a2fd30183bdf6bbdf
Dan Dela Rosa <dandelarosa@Dans-MBP.nyc.rr.com>
2017-01-07 14:00:45 -0500

    Add placeholder images for tiles

--- game/bricks/tileimages.js
+++ game/bricks/tileimages.js
@@ -65,6 +65,20 @@ function filenameForKey(key) {
     case TILE_PIPE_MID_RIGHT: return 'pipe-midright.png';
     case TILE_FLAGPOLE_TOP: return 'flagpole-top.png';
     case TILE_FLAGPOLE_MID: return 'flagpole-middle.png';
+    case TILE_UNDERGROUND_BG: return 'underground/bg.png';
+    case TILE_BLUE_ROCKS: return 'underground/rocks.png';
+    case TILE_BLUE_BLOCK: return 'underground/block.png';
+    case TILE_BLUE_BRICK_BLOCK: return 'underground/bricks.png';
+    case TILE_UNDERGROUND_PIPE_TOP_END_LEFT: return 'underground/pipe-topleft.png';
+    case TILE_UNDERGROUND_PIPE_TOP_END_RIGHT: return 'underground/pipe-topright.png';
+    case TILE_UNDERGROUND_PIPE_V_LEFT: return 'underground/pipe-vleft.png';
+    case TILE_UNDERGROUND_PIPE_V_RIGHT: return 'underground/pipe-vright.png';
+    case TILE_UNDERGROUND_PIPE_H_TOP: return 'underground/pipe-htop.png';
+    case TILE_UNDERGROUND_PIPE_H_BOTTOM: return 'underground/pipe-hbottom.png';
+    case TILE_UNDERGROUND_PIPE_LEFT_END_TOP: return 'underground/pipe-lefttop.png';
+    case TILE_UNDERGROUND_PIPE_LEFT_END_BOTTOM: return 'underground/pipe-leftbottom.png';
+    case TILE_UNDERGROUND_PIPE_LEFT_INT_TOP: return 'underground/pipe-leftinttop.png';
+    case TILE_UNDERGROUND_PIPE_LEFT_INT_BOTTOM: return 'underground/pipe-leftintbottom.png';
   }
 }
 
