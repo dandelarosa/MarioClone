b07831cb47023dbecc0045024b74ade5eeda49b0
Dan Dela Rosa <dandelarosa@Dans-MBP.nyc.rr.com>
2017-01-22 16:07:00 -0500

    Delete deprecated tile types from the menu

--- editor/tilepicker.js
+++ editor/tilepicker.js
@@ -25,22 +25,6 @@ function createTilePicker() {
     });
   });
   var plain_tiles = [
-    {value: TILE_BLUE_SKY, displayName: 'Blue Sky'},
-    {value: TILE_BROWN_GROUND, displayName: 'Brown Ground'},
-    {value: TILE_CLOUD_UL, displayName: 'Cloud Upper Left'},
-    {value: TILE_CLOUD_UC, displayName: 'Cloud Upper Center'},
-    {value: TILE_CLOUD_UR, displayName: 'Cloud Upper Right'},
-    {value: TILE_CLOUD_LL, displayName: 'Cloud Lower Left'},
-    {value: TILE_CLOUD_LC, displayName: 'Cloud Lower Center'},
-    {value: TILE_CLOUD_LR, displayName: 'Cloud Lower Right'},
-    {value: TILE_BUSH_LEFT, displayName: 'Bush Left'},
-    {value: TILE_BUSH_CENTER, displayName: 'Bush Center'},
-    {value: TILE_BUSH_RIGHT, displayName: 'Bush Right'},
-    {value: TILE_HILL_LEFT, displayName: 'Hill - Left Slope'},
-    {value: TILE_HILL_TOP, displayName: 'Hill - Top'},
-    {value: TILE_HILL_RIGHT, displayName: 'Hill - Right'},
-    {value: TILE_HILL_EYES, displayName: 'Hill - Eyes'},
-    {value: TILE_HILL_NOEYES, displayName: 'Hill - No Eyes'},
     {value: TILE_CASTLE_TOP, displayName: 'Castle - Top'},
     {value: TILE_CASTLE_WINDOWLEFT, displayName: 'Castle - Window Left'},
     {value: TILE_CASTLE_WINDOWRIGHT, displayName: 'Castle - Window Right'},
