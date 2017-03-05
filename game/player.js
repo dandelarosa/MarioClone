21512354f0dbaf1625b45d38f969472df7bfbe72
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2017-03-05 18:12:59 -0500

    Fix vertical collisions again

--- game/player.js
+++ game/player.js
@@ -58,24 +58,32 @@ Player.prototype.move = function(keyboard, bricks) {
   var leftX = this.x;
   var rightX = this.x + this.width;
   var futureTopY = this.y + this.speedY;
+  var futureBottomY = this.y + this.height + this.speedY;
   var futureLeftX = this.x + this.speedX;
   var futureRightX = this.x + this.width + this.speedX;
   // If future top side is inside a wall, push to row below
-  if (this.speedY < 0
-    && (bricks.isSolidAtPoint(this.x, futureTopY) || bricks.isSolidAtPoint(rightX, futureTopY))) {
+  if (this.speedY < 0 && bricks.isSolidAtPoint(this.x, futureTopY)) {
     this.y = Math.floor(this.y / bricks.grid.cellHeight) * bricks.grid.cellHeight;
     this.speedY = 0.0;
   }
-
-   if (this.speedY > 0 &&
-       bricks.isSolidAtPoint(this.x, this.y + JUMPER_RADIUS) == 1) {
-     this.y = (1 + Math.floor(this.y / bricks.grid.cellHeight)) * bricks.grid.cellHeight - JUMPER_RADIUS;
-     this.onGround = true;
-     this.speedY = 0;
-   }
-   else if (bricks.isSolidAtPoint(this.x, this.y + JUMPER_RADIUS + 2) == 0) {
-     this.onGround = false;
-   }
+  else if (this.speedY < 0 && bricks.isSolidAtPoint(rightX - 1, futureTopY)) {
+    this.y = Math.floor(this.y / bricks.grid.cellHeight) * bricks.grid.cellHeight;
+    this.speedY = 0.0;
+  }
+  // If future bottom side is inside a wall, push to row above
+  else if (this.speedY > 0 && bricks.isSolidAtPoint(leftX, futureBottomY)) {
+    this.y = (Math.floor(futureBottomY / bricks.grid.cellHeight)) * bricks.grid.cellHeight - this.height;
+    this.onGround = true;
+    this.speedY = 0;
+  }
+  else if (this.speedY > 0 && bricks.isSolidAtPoint(rightX - 1, futureBottomY)) {
+    this.y = (Math.floor(futureBottomY / bricks.grid.cellHeight)) * bricks.grid.cellHeight - this.height;
+    this.onGround = true;
+    this.speedY = 0;
+  }
+  else if (bricks.isSolidAtPoint(this.x, this.y + this.height + 2) == 0) {
+    this.onGround = false;
+  }
 
   // If left side is already inside a wall, push to the column to the right
   if (this.speedX < 0
