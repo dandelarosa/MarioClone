2e54cbff1d3da974aa00f9bcc32216699726b7a9
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2017-04-09 16:38:36 -0400

    Create Persistence class

--- utils/persistence.js
+++ utils/persistence.js
@@ -13,3 +13,42 @@ function getStoredValue(key, fallbackValue) {
   }
   return window.localStorage.getItem(key);
 }
+
+function Persistence() {
+  return {
+    getValue: getValue,
+    setValue: setValue,
+  };
+
+  function getValue(key, type, fallbackValue) {
+    // This is a workaround for DOMException: Failed to read the 'localStorage'
+    // property from 'Window': Access is denied for this document.
+    try {
+      var value = window.localStorage.getItem(key);
+      if (value === undefined) {
+        return fallbackValue;
+      }
+      if (value === null) {
+        return fallbackValue;
+      }
+      if (type === 'int') {
+        return parseInt(value);
+      }
+      return value;
+    }
+    catch(e) {
+      return fallbackValue;
+    }
+  }
+
+  // Returns true if the value was saved successfully
+  function setValue(key, value) {
+    try {
+      window.localStorage.setItem(key, value);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
+  }
+}
