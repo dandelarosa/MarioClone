c18e705936f4582660877cc40eccfffed0ecfc04
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2017-04-25 13:58:10 -0400

    Add some properties to EditorMode

--- game/modes/editor_mode.js
+++ game/modes/editor_mode.js
@@ -1,10 +1,37 @@
-function EditorMode() {
+function EditorMode(game, tiles) {
+  this.scaleX = 2;
+  this.scaleY = 2;
+  this.tiles = tiles;
+  this.levelMockupAlpha = 0.0;
+
+  this.tileEditingMode = new TileEditingMode();
+  this.obstacleEditingMode = new ObstacleEditingMode();
+  var savedEditingMode = persistence.getValue('editingMode', 'int', EDITING_MODE.TILES);
+  if (savedEditingMode === EDITING_MODE.OBSTACLES) {
+    this.currentEditingMode = this.obstacleEditingMode;
+  }
+  else {
+    this.currentEditingMode = this.tileEditingMode;
+  }
+
+  // To initialize later
+  this.editorCamera = null;
+  this.levelImageKey = null;
+  this.levelImageOffset = null;
 }
 
 EditorMode.prototype = (function() {
   return {
     draw: draw,
+    loadWorld: loadWorld,
+    reset: reset,
     update: update,
+  }
+
+  function loadWorld() {
+  }
+
+  function reset() {
   }
 
   function update() {
