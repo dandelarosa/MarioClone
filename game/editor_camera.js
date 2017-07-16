ea275c888d64094850d6fcf14bd87afe04b54c37
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2017-07-16 19:34:35 -0400

    Make EditorCamera more like PlayerCamera

--- game/editor_camera.js
+++ game/editor_camera.js
@@ -1,4 +1,4 @@
-const CAMERA_SPEED = 16;
+const EDITOR_CAMERA_SPEED = 16;
 
 function EditorCamera(x, y, width, height) {
   this.x = x;
@@ -7,36 +7,56 @@ function EditorCamera(x, y, width, height) {
   this.height = height;
 }
 
-EditorCamera.prototype.update = function(keyboard, tiles) {
-  var nextX = this.x;
-  var nextY = this.y;
+EditorCamera.prototype = (function() {
+  return {
+    getRect: getRect,
+    update: update,
+  };
 
-  if (keyboard.isKeyPressed(KEY_LEFT_ARROW)) {
-    nextX -= CAMERA_SPEED;
-  }
-  if (keyboard.isKeyPressed(KEY_RIGHT_ARROW)) {
-    nextX += CAMERA_SPEED;
-  }
-  if (keyboard.isKeyPressed(KEY_UP_ARROW)) {
-    nextY -= CAMERA_SPEED;
-  }
-  if (keyboard.isKeyPressed(KEY_DOWN_ARROW)) {
-    nextY += CAMERA_SPEED;
+  /**
+   * Gets the object's rect value.
+   * @returns {Object} The object's rect.
+   */
+  function getRect() {
+    return new Rect2D(this.x, this.y, this.width, this.height);
   }
 
-  if (nextX < tiles.minX()) {
-    nextX = tiles.minX();
-  }
-  if (nextY < tiles.minY()) {
-    nextY = tiles.minY();
-  }
-  if (nextX + this.width > tiles.maxX()) {
-    nextX = tiles.maxX() - this.width;
-  }
-  if (nextY + this.height > tiles.maxY()) {
-    nextY = tiles.maxY() - this.height;
-  }
+  /**
+   * Updates the camera's position
+   * @param {Object} keyboard - The keyboard controlling the camera.
+   * @param {Object} tiles - The object with the bounds data.
+   */
+  function update(keyboard, tiles) {
+    var nextX = this.x;
+    var nextY = this.y;
 
-  this.x = nextX;
-  this.y = nextY;
-};
+    if (keyboard.isKeyPressed(KEY_LEFT_ARROW)) {
+      nextX -= EDITOR_CAMERA_SPEED;
+    }
+    if (keyboard.isKeyPressed(KEY_RIGHT_ARROW)) {
+      nextX += EDITOR_CAMERA_SPEED;
+    }
+    if (keyboard.isKeyPressed(KEY_UP_ARROW)) {
+      nextY -= EDITOR_CAMERA_SPEED;
+    }
+    if (keyboard.isKeyPressed(KEY_DOWN_ARROW)) {
+      nextY += EDITOR_CAMERA_SPEED;
+    }
+
+    if (nextX < tiles.minX()) {
+      nextX = tiles.minX();
+    }
+    if (nextY < tiles.minY()) {
+      nextY = tiles.minY();
+    }
+    if (nextX + this.width > tiles.maxX()) {
+      nextX = tiles.maxX() - this.width;
+    }
+    if (nextY + this.height > tiles.maxY()) {
+      nextY = tiles.maxY() - this.height;
+    }
+
+    this.x = nextX;
+    this.y = nextY;
+  }
+})();
